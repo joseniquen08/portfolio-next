96121f189f2a318604aeab44b29befdc2dd7c5a9
joseniquen08 <j.armando0807@gmail.com>
2022-03-14 16:11:36 -0500

    fix: api projects fixed

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -5,12 +5,12 @@ export default async function getProjects (_req, res) {
   const data = await firestore.collection('projects').get();
 
   if (!data) {
-    res.status(404).json({});
+    return res.status(404).json({});
   }
 
   data.forEach(project => {
     projects.push({ id: project.id, ...project.data()});
   });
 
-  res.status(200).json({ projects });
+  return res.status(200).json({ projects });
 }
