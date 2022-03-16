a1a38694fa450965c37ea211da1aac876129c4f7
joseniquen08 <j.armando0807@gmail.com>
2022-03-16 16:39:48 -0500

    feat: logo added

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>José Ñiquen</title>
         <meta name="description" content="My name is José Ñiquen and this is my web portfolio." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <Component {...pageProps} />
     </>
